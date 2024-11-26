1c8fecb9f69172fe8715e88cdd43e30ba4e1242a
femke77 <megan.meyers.388@gmail.com>
2024-11-26 00:20:31 -0800

    comments render with single blog

--- client/src/interfaces/Blog.ts
+++ client/src/interfaces/Blog.ts
@@ -1,7 +1,10 @@
+import type { IComment } from "./Comment";
+
 export interface IBlog {
   _id: string;
   title: string;
   content: string;
   username: string;
   dateCreated: string;
+  comments?: IComment[] | null | [];
 }
--- client/src/pages/SingleBlog.tsx
+++ client/src/pages/SingleBlog.tsx
@@ -1,8 +1,37 @@
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { GET_BLOG } from "../utils/queries";
+import Blog from "../components/Blog";
+import { IBlog } from "../interfaces/Blog";
+
 const SingleBlog = () => {
+
+    const { blogId } = useParams();
+    const { loading, data, error } = useQuery(GET_BLOG, { variables: { blogId: blogId } });
+    const blog: IBlog = data?.blog || {};
+    if (loading) {
+        return <div>Loading...</div>
+    }
     return (
         <div>
-            <h1 className="display-2">SingleBlog</h1>
-            <p>Welcome to the single blog w/ comments page!</p>
+            <Blog
+                key={blog._id}
+                username={blog.username}
+                content={blog.content}
+                dateCreated={blog.dateCreated}
+                title={blog.title}
+                _id={blog._id}
+
+            />
+            {blog.comments && blog.comments.map((comment) => (
+                <>
+                    <hr />
+                    <div key={comment._id}>
+                        <p>{comment.comment}</p>
+                        <p>By: {comment.username} on {comment.dateCreated}</p>
+                    </div></>
+            ))}
+            {error && <div>Error: {error.message}</div>}
         </div>
     )
 }
